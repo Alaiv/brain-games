c72a3159df5d614fba125a1e82612de4d101a7e3
Kudriavtsev Aleksandr Sergeevich <lasacc4@yandex.ru>
2022-09-28 11:02:13 +0300

    fixed greeting

--- src/welcome.js
+++ src/welcome.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const getUserName = (message) => {
-  console.log('Welcome to Brain Games!');
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(message);
